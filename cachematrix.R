1d8d8f82f8ac55e11183dbd08569e9608c71d0ad
Todd Jenista <tjenista@gmail.com>
2015-04-25 12:31:10 -0400

    Program Assignment 2 - solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ## Purpose:  Generate two file that work together to calculate
+## The inverse of a matrix with a OPTIMIZATION of the program
+## storing and recalling the inverse matrix to substantially
+## decrease calculation time (solve once use forever).
 
-## Write a short comment describing this function
-
+## Function: makeCacheMatrix  - "Class"
+## 
+## Input:   x             matrix
+##
+## Output:  Function      Purpose
+##          get           Gets the original matrix
+##          setInverse    Sets the stored inverse matrix 
+##          getInverse    Gets the stored inverse matrix (NULL if none)
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solved) s <<- solved
+  getInverse <- function() s
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Function: makeCacheMatrix  - Return a matrix that is the inverse of 'x'
+## 
+## Input:   x             matrix
+##
+## Output:  s             inverse maxtrix of x
+cacheSolve <- function(x, ...) {        
+  s <- x$getInverse()
+  if(!is.null(s)) {               ## check if inverse is already stored
+    message("getting cached data")
+  }
+  s <- solve(data, ...)
+  x$setInverse(s)
+  s
 }
